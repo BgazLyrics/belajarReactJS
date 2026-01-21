d36aed14d8ceabb25dd06cc4db7db286086f9ff2
BgazLyrics <bagasseviardanarionaldi@gmail.com>
2026-01-21 09:09:37 +0700

    Menambahkan styling CSS card untuk biodata

--- pertemuanpertama/src/App.jsx
+++ pertemuanpertama/src/App.jsx
@@ -3,31 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Greeting() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>HAlloo</h1>
+      <p>Selamat pagi</p>
+    </>
+  )
+}
+
+function Biodata(props) {
+  return (
+    <div className="card">
+      <p><strong>Nama:</strong> {props.nama}</p>
+      <p><strong>Kelas:</strong> {props.kelas}</p>
+      <p><strong>NISN:</strong> {props.nisn}</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Greeting />
+      <div className="card-container">
+        <Biodata nama="Bagas" kelas="XI PPLG 1" nisn="2126202525" />
+        <Biodata nama="Cuscus" kelas="XI PPLG 1" nisn="856696552" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
